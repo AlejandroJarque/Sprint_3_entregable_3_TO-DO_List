a6ab35f14d1786219dcac1fe3056c35024f7bdd3
Jaki Riggs <jarquealex0302@gmail.com>
2025-11-19 20:18:46 +0100

    defining indexAction

--- app/controllers/CategoriesController.php
+++ app/controllers/CategoriesController.php
@@ -4,6 +4,8 @@ class CategoriesController extends ApplicationController {
 
     public function indexAction() {
 
+        $categoryModel = new Category();
+        $this->view->categories = $categoryModel->getAll();
     }
 
     public function createAction() {
