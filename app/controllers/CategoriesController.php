02b0743c22cfdb287987716318a4e826fbafd323
Jaki Riggs <jarquealex0302@gmail.com>
2025-11-19 18:21:05 +0100

    create class CategoriesController

---
+++ app/controllers/CategoriesController.php
@@ -0,0 +1,29 @@
+<?php
+
+class CategoriesController extends ApplicationController {
+
+    public function indexAction() {
+
+    }
+
+    public function createAction() {
+
+    }
+
+    public function storeAction() {
+
+    }
+
+    public function editAction() {
+
+    }
+
+    public function updateAction() {
+
+    }
+
+    public function deleteAction() {
+        
+    }
+}
+?>
