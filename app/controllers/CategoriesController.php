c4940c7c6ed404312e2656f10981a9271394803f
Jaki Riggs <jarquealex0302@gmail.com>
2025-11-19 20:24:31 +0100

    defining storeAction

--- app/controllers/CategoriesController.php
+++ app/controllers/CategoriesController.php
@@ -14,6 +14,24 @@ class CategoriesController extends ApplicationController {
 
     public function storeAction() {
 
+        if($_SERVER['REQUEST_METHOD'] !== 'POST') {
+            throw new Exception("Metodo no permitido");
+        }
+
+        $name = trim($_POST['categori_name'] ?? '');
+        $description = trim($_POST['categori_description'] ?? '');
+
+        if($name === '' || $description === '') {
+             
+            header('Location: ' . WEB_ROOT . '/categories/create');
+            exit;
+        }
+
+        $categoryModel = new Category();
+        $categoryModel -> insertCategory($name, $description);
+
+        header('Location: ' . WEB_ROOT . '/categories');
+        exit;
     }
 
     public function editAction() {
