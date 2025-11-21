13c17e594926ddd54903df13f5a03d04ed243439
Jaki Riggs <jarquealex0302@gmail.com>
2025-11-21 14:12:18 +0100

    adding setLayout to CategoriesController

--- app/controllers/CategoriesController.php
+++ app/controllers/CategoriesController.php
@@ -4,6 +4,7 @@ class CategoriesController extends ApplicationController {
 
     public function indexAction() {
 
+        $this->view->setLayout('main');
         $categoryModel = new Category();
         $this->view->categories = $categoryModel->getAll();
     }
