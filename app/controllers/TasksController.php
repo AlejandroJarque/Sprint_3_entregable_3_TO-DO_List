8b93c5fab06cf628665f42520b4b8252daa8ad0c
Hebe Stark <hebe_s@hotmail.com>
2025-11-24 11:07:46 +0100

    Creo archivo Task controller

---
+++ app/controllers/TasksController.php
@@ -0,0 +1,24 @@
+<?php
+class TasksController extends ApplicationController {
+
+    public function indexAction(){
+
+    }
+    public function createAction(){
+
+    }
+    public function storeAction(){
+
+    }
+    public function updateAction(){
+
+    }
+    public function deleteAction(){
+        
+    }
+    public function editAction(){
+
+    }
+
+}
+?>
