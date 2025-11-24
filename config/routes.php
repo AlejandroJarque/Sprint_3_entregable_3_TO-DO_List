38a8979fccb3e03e7c996bfb843bf2d5c393d94a
Hebe Stark <hebe_s@hotmail.com>
2025-11-24 11:02:41 +0100

    Definicion de rutas

--- config/routes.php
+++ config/routes.php
@@ -1,14 +1,12 @@
 <?php 
 
-/**
- * Used to define the routes in the system.
- * 
- * A route should be defined with a key matching the URL and an
- * controller#action-to-call method. E.g.:
- * 
- * '/' => 'index#index',
- * '/calendar' => 'calendar#index'
- */
 $routes = array(
 	'/test' => 'test#index'
 );
+$routes['/tasks'] = 'Tasks#index';
+$routes['/tasks/index'] = 'Tasks#index';
+$routes['/tasks/create'] = 'Tasks#create';
+$routes['/tasks/store'] = 'Tasks#store';
+$routes['/tasks/edit'] = 'Tasks#edit';
+$routes['/tasks/update'] = 'Tasks#update';
+$routes['/tasks/delete'] = 'Tasks#delete';
