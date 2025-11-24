aa26b84b6d59bc15e166c5344507819f4682758e
Hebe Stark <hebe_s@hotmail.com>
2025-11-24 12:16:00 +0100

    Creo la clase Task

---
+++ app/models/Task.php
@@ -0,0 +1,33 @@
+<?php
+class Task {
+private $file;
+
+public function __construct(){
+    $this->file = ROOT_PATH . 'app/data/tasks.json';
+}
+
+    private function load(){
+
+    }
+    private function save(){
+
+    }
+    
+    public function getAll(){
+
+    }
+    public function getById(){
+
+    }
+    public function insertTask(){
+
+    }
+    public function updateTask(){
+
+        }
+    public function deleteTask(){
+
+    }
+
+}
+?>
